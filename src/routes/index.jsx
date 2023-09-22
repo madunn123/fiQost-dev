e753f2ba6dc9fc29bcf50a5d16deb05fefe753b4
madunn123 <donquixoteramadhan9@gmail.com>
2023-09-22 10:02:01 +0700

    setup layout

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,10 +1,11 @@
 /* eslint-disable import/prefer-default-export */
+import Login from '@/pages/test';
 import { createBrowserRouter } from 'react-router-dom';
 
 export const router = createBrowserRouter([
   {
     path: '/',
-    element: <div className="bg-red-600 text-white">hehehe</div>,
+    element: <Login />,
   },
 ]);
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    './index.html',
-    './src/**/*.{js,ts,jsx,tsx}',
-  ],
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'main-color': '#7879F1',
+        'secondary-color': '#AFA2C3',
+        'font-colors': '#3E334E',
+        'thirdy-color': '#AEAFF7',
+      },
+      accentColor: {
+        'main-accent': '#7879F1',
+      },
+      fontFamily: {
+        sans: ['Inter', 'sans'],
+      },
+    },
   },
   plugins: [],
 };
